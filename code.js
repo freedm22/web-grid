943046f3260a076e4429433147249e8971225b50
freedm <freedm@orange.fr>
2023-03-01 15:43:29 +0100

    random colors + color new cells

--- code.js
+++ code.js
@@ -4,21 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initial clean up. DO NOT REMOVE.
   initialCleanup();
 
-  /** to add a line when butten is clicked */
+  /** to add a line when button is clicked */
   document.getElementById("btn-add-line").addEventListener("click",
     () => {
       for (let i=0; i<10; i++)
       {
         div = document.createElement("div");
-        document.getElementById("grid").append(div)
+        document.getElementById("grid").append(div);
+        div.addEventListener("click",(event)=>
+    {
+      event.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase()
+    }    
+    )
       }
     });
 
+  /**to change the color of a square*/
   for (let square of document.getElementById("grid").children)
   {
     square.addEventListener("click",(event)=>
     {
-      event.target.style.backgroundColor="blue"
+      event.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase()
     }    
     )
   }
